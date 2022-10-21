0aa91e3c949a6d79a9c0f8b3815e55a8e1751606
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-10-21 17:06:08 +0700

    add input method using while function

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -9,6 +9,32 @@ namespace QuickSort
         private int mov_count = 0;
 
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Masukan elemen array");
+                Console.WriteLine("----------------------------");
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
